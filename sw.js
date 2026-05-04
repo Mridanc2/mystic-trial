dc9ce0be1c63430c67eea8396d4a98477263cee2
Mridanc2 <mr.idanc@gmail.com>
2026-05-04 15:54:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mystic-trial-v3.5";
+const CACHE = "mystic-trial-v3.6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
